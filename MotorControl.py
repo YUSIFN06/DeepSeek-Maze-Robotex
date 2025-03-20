d2a4bb163df7cba91e62be03934c302c1dc2b925
userAAR <amil.abdullazada@gmail.com>
2025-03-20 15:51:33 +0400

    Optimized Motor Controller and integrated with main logic

--- MotorControl.py
+++ MotorControl.py
@@ -4,58 +4,62 @@ import machine, time
 
 class MotorControl():
 
-    def __init__(self, enable_pins, motor_pins, speed):
+    def __init__(self, enable_pins, motor_pins, sensor_pins, speed):
+        # Motor PWM pins
         self.right_motor_enable_pin = PWM(Pin(enable_pins[0]), freq=2000)
         self.left_motor_enable_pin = PWM(Pin(enable_pins[1]), freq=2000)
 
+        # Motor direction pins
         self.left_motor_forward = Pin(motor_pins[0], Pin.OUT)
         self.left_motor_backward = Pin(motor_pins[1], Pin.OUT)
-
         self.right_motor_forward = Pin(motor_pins[2], Pin.OUT)
         self.right_motor_backward = Pin(motor_pins[3], Pin.OUT)
 
+        # IR Sensors
+        self.front_sensor = Pin(sensor_pins[0], Pin.IN)
+        self.left_sensor = Pin(sensor_pins[1], Pin.IN)
+        self.right_sensor = Pin(sensor_pins[2], Pin.IN)
+
+        # Default speed
         self.speed = speed
         self.update_speed()
 
-    def update_speed(self):
-        self.right_motor_enable_pin.duty_u16(self.speed)
-        self.left_motor_enable_pin.duty_u16(self.speed)
+    def read_sensors(self):
+        return {
+            "front": self.front_sensor.value(),
+            "left": self.left_sensor.value(),
+            "right": self.right_sensor.value()
+        }
+
+    def update_speed(self, stop = False):
+        if stop:
+            self.right_motor_enable_pin.duty_u16(0)
+            self.left_motor_enable_pin.duty_u16(0)
+        else:
+            self.right_motor_enable_pin.duty_u16(self.speed)
+            self.left_motor_enable_pin.duty_u16(self.speed)
+
+    def move(self, left_dir, right_dir, stop = False):
+        self.update_speed(stop)
+        self.left_motor_forward.value(left_dir[0])
+        self.left_motor_backward.value(left_dir[1])
+        self.right_motor_forward.value(right_dir[0])
+        self.right_motor_backward.value(right_dir[1])
 
     def forward(self):
-        self.update_speed()
-        self.left_motor_forward.value(1)
-        self.left_motor_backward.value(0)
-        self.right_motor_forward.value(1)
-        self.right_motor_backward.value(0)
+        self.move((1, 0), (1, 0))
 
     def stop(self):
-        self.left_motor_forward.value(0)#
-        self.left_motor_backward.value(0)
-        self.right_motor_forward.value(0)
-        self.right_motor_backward.value(0)
-        self.right_motor_enable_pin.duty_u16(0)
-        self.left_motor_enable_pin.duty_u16(0)
+        self.move((0, 0), (0, 0),True)
 
     def turnRight(self):
-        self.update_speed()
-        self.left_motor_forward.value(0)
-        self.left_motor_backward.value(1)
-        self.right_motor_forward.value(1)
-        self.right_motor_backward.value(0)
+        self.move((0, 1), (1, 0))
 
     def turnLeft(self):
-        self.update_speed()
-        self.left_motor_forward.value(1)
-        self.left_motor_backward.value(0)
-        self.right_motor_forward.value(0)
-        self.right_motor_backward.value(1)
+        self.move((1, 0), (0, 1))
 
     def turnBack(self):
-        self.update_speed()
-        self.left_motor_forward.value(0)
-        self.left_motor_backward.value(1)
-        self.right_motor_forward.value(1)
-        self.right_motor_backward.value(0)
+        self.move((0, 1), (1, 0))
 
     def setSpeed(self, new_speed):
         self.speed = new_speed
--- main.py
+++ main.py
@@ -1,110 +1,25 @@
-from robot import Robot
-from factory import MazeSolverFactory
+from MotorControl import MotorControl
+import time
+
 
 enable_pins = [17, 16]
 motor_pins = [19, 18, 5, 4]
 sensor_pins = [26, 27, 32]
 
-robot = Robot(enable_pins, motor_pins, sensor_pins)
-
-selected_algorithm = "dfs"
-
-solver = MazeSolverFactory.get_solver(selected_algorithm)
-if solver:
-    robot.execute_algorithm(solver)
-else:
-    print("Wrong Algorithm!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from time import sleep
-#
-# from MotorControl import MotorControl
-#
-# enable_pins = [17, 16]
-# motor_pins = [19, 18, 5, 4]
-#
-# robot = MotorControl(enable_pins, motor_pins, speed=40000)
-#
-# robot.forward()
-# sleep(2)
-#
-# robot.turnRight()
-# sleep(1)
-#
-# robot.turnLeft()
-# sleep(1)
-#
-# robot.turnBack()
-# sleep(2)
-#
-# robot.stop()
+robot = MotorControl(enable_pins, motor_pins, sensor_pins, speed=40000)
+
+def right_hand_rule():
+    while True:
+        sensors = robot.read_sensors()
+
+        if sensors["right"] == 1:
+            robot.turnRight()
+            time.sleep(0.5)
+        elif sensors["front"] == 1:
+            robot.turnLeft()
+            time.sleep(0.5)
+        else:
+            robot.forward()
+            time.sleep(0.5)
+
+right_hand_rule()
